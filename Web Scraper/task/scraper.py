385b07d4ac59a4e83c8706d6763568ddbacba2d1
MukendiMputu <mputusam@live.com>
2021-03-28 17:30:37 +0200

    finishes Stage 3

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 usr_url = input()
+source_file = open('source.html', 'wb')
 
 
 def get_url(url):
@@ -13,15 +14,18 @@ def get_soup(http_response):
     return BeautifulSoup(http_response.content, 'html.parser')
 
 
+def get_data_byte(http_res):
+    return http_res.content
+
+
 response = get_url(usr_url)
-if response.status_code:
+if response.status_code in range(200, 400):
     soup = get_soup(response)
-    movie_title = soup.find('title')
-    description = soup.find('div', {'class': 'summary_text'})
-    if not (movie_title and description):
-        print("Invalid movie page!")
+    page_content = get_data_byte(response)
+    if source_file.write(page_content):
+        print("Content saved.")
     else:
-        result = {"title": movie_title.text, "description": description.text.strip()}
-        print(result)
+        print("Error writing byte to file")
 else:
-    print(response.status_code, " - Error")
+    print(f"The URL returned {response.status_code}!")
+source_file.close()
