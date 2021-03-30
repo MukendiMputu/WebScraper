3ee3239a4849f72b2651a50b90e04322552289e5
MukendiMputu <mputusam@live.com>
2021-03-30 10:23:45 +0200

    working on Stage 4

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -2,8 +2,8 @@ import requests
 
 from bs4 import BeautifulSoup
 
-usr_url = input()
-source_file = open('source.html', 'wb')
+usr_url = "https://www.nature.com/nature/articles"
+# source_file = open('source.html', 'wb')
 
 
 def get_url(url):
@@ -18,14 +18,30 @@ def get_data_byte(http_res):
     return http_res.content
 
 
+def clean_title(title_list):
+    clean_list = [title.replace(" ", "_") for title in title_list]
+    return clean_list
+
+
 response = get_url(usr_url)
 if response.status_code in range(200, 400):
     soup = get_soup(response)
-    page_content = get_data_byte(response)
-    if source_file.write(page_content):
-        print("Content saved.")
-    else:
-        print("Error writing byte to file")
+    article_titles = [anchor.text for anchor in
+                      soup.find_all('a', {'data-track-action': 'view article'})
+                      ]
+    article_links = [anchor.attrs['href'] for anchor in
+                     soup.find_all('a', {'data-track-action': 'view article'})
+                     ]
+    article_types = [type_text.text.strip() for type_text in
+                     soup.find_all('span', {'data-test': 'article.type'})
+                     ]
+    print(article_titles)
+    print(article_types)
+    # page_content = get_data_byte(response)
+    # if source_file.write(page_content):
+    #     print("Content saved.")
+    # else:
+    #     print("Error writing byte to file")
 else:
     print(f"The URL returned {response.status_code}!")
-source_file.close()
+# source_file.close()
