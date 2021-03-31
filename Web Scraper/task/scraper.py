587b30956468c050eb9de59906078f24d36925fc
MukendiMputu <mputusam@live.com>
2021-03-31 17:39:09 +0200

    finished stage 5

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,9 +1,10 @@
-import requests
+import os
+import string
 
+import requests
 from bs4 import BeautifulSoup
 
 usr_url = "https://www.nature.com/nature/articles"
-# source_file = open('source.html', 'wb')
 
 
 def get_url(url):
@@ -23,25 +24,39 @@ def clean_title(title_list):
     return clean_list
 
 
-response = get_url(usr_url)
-if response.status_code in range(200, 400):
-    soup = get_soup(response)
-    article_titles = [anchor.text for anchor in
-                      soup.find_all('a', {'data-track-action': 'view article'})
-                      ]
-    article_links = [anchor.attrs['href'] for anchor in
-                     soup.find_all('a', {'data-track-action': 'view article'})
-                     ]
-    article_types = [type_text.text.strip() for type_text in
-                     soup.find_all('span', {'data-test': 'article.type'})
-                     ]
-    print(article_titles)
-    print(article_types)
-    # page_content = get_data_byte(response)
-    # if source_file.write(page_content):
-    #     print("Content saved.")
-    # else:
-    #     print("Error writing byte to file")
-else:
-    print(f"The URL returned {response.status_code}!")
-# source_file.close()
+numb_of_pages = int(input())
+type_of_articles = input()
+
+for page in range(1, numb_of_pages + 1):
+    response = get_url(usr_url + '?searchType=journalSearch&sort=PubDate&page=' + str(page))
+    dir_name = 'Page_' + str(page)
+    os.mkdir(dir_name)
+    if response.status_code in range(200, 400):
+        soup = get_soup(response)
+        article_links = [anchor.attrs['href'] for anchor in
+                         soup.find_all('a', {'data-track-action': 'view article'})]
+        for link in article_links:
+            page_link = "https://www.nature.com"
+            page_link += link
+            r = get_url(page_link)  # get content in article page
+            if r.status_code in range(200, 400):
+                s = get_soup(r)
+                if s.find('meta', {'name': 'dc.type', 'content': type_of_articles}):
+                    replace_punctuation = str.maketrans('', '', string.punctuation)
+                    file_name = s.find('h1', {'class': 'article-item__title', 'itemprop': 'headline'}) \
+                        .text.strip() \
+                        .translate(replace_punctuation) \
+                        .replace(' ', '_')
+                    file_name = os.path.join(dir_name, file_name)
+                    file_content = s.find('div', {'class': 'article-item__body'}) \
+                        or s.find('div', {'class': 'article__body'}) \
+                        if s.find('div', {'class': 'article-item__body'}) \
+                        or s.find('div', {'class': 'article__body'}) else ''
+                    if file_content:
+                        file_content = file_content.text.strip()
+                    source_file = open(f'{file_name}.txt', 'wb')
+                    source_file.write(bytes(file_content, 'utf-8'))
+                    source_file.close()
+    else:
+        print(f"The URL returned {response.status_code}!")
+print("Saved all articles.")
